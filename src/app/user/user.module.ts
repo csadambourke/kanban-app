caaf8f72bb02bf651fadfc7bd35c27df6fc2c369
Adam Bourke <csadambourke@gmail.com>
2022-06-29 19:43:41 +1000

    added email component

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -3,11 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { UserRoutingModule } from './user-routing.module';
 import { LoginPageComponent } from './login-page/login-page.component';
+import { GoogleSigninDirective } from './google-signin.directive';
+import { EmailLoginComponent } from './email-login/email-login.component';
 
 
 @NgModule({
   declarations: [
-    LoginPageComponent
+    LoginPageComponent,
+    GoogleSigninDirective,
+    EmailLoginComponent
   ],
   imports: [
     CommonModule,
